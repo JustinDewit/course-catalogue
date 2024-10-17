f6a32c07ce5025378eaab8c64680009d5b538dfd
JustinDewit <137382666+JustinDewit@users.noreply.github.com>
2024-10-17 15:18:01 +0800

    make basic CourseList component

---
+++ src/components/CourseList.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const CourseList = () => {
+  const [courses, setCourses] = useState([]);
+
+  return <div>CourseList</div>;
+};
+
+export default CourseList;
