e0b4b1481d55bfd5ae1ef774c75362b36bbe2c2b
JustinDewit <137382666+JustinDewit@users.noreply.github.com>
2024-10-18 13:02:27 +0800

    feat: display courses

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,9 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import coursesData from "../data/courses.json";
+import { Course } from "../types/types";
+
+const typedCoursesData = coursesData as Course[];
 
 const CourseList = () => {
-  const [courses, setCourses] = useState([]);
+  const [courses, setCourses] = useState<Course[]>([]);
 
-  return <div>CourseList</div>;
+  useEffect(() => {
+    setCourses(typedCoursesData);
+  }, []);
+
+  return (
+    <div>
+      <h1>Available Courses</h1>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.id}>
+            <h2>{course.title}</h2>
+            <p>{course.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default CourseList;
